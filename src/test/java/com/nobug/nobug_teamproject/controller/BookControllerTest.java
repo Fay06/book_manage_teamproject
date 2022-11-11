d1e08ae327ed87a49577c9d45d507e9362417dcb
serenawcy <serenawangcy@gmail.com>
2022-11-11 12:46:53 -0500

    Add unit test structure

---
+++ src/test/java/com/nobug/nobug_teamproject/controller/BookControllerTest.java
@@ -0,0 +1,34 @@
+package com.nobug.nobug_teamproject.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getBook() {
+    }
+
+    @Test
+    void deleteBookId() {
+    }
+
+    @Test
+    void addBook() {
+    }
+
+    @Test
+    void updateBook() {
+    }
+}
