e28577261f30c44bd50f3a2642952b35f1e2b3c8
Yufei Liu <yliu1224@umd.edu>
2022-11-11 20:15:04 -0500

    finish book service test

--- src/test/java/com/nobug/nobug_teamproject/service/BookServiceTest.java
+++ src/test/java/com/nobug/nobug_teamproject/service/BookServiceTest.java
@@ -1,12 +1,21 @@
 package com.nobug.nobug_teamproject.service;
 
+import com.nobug.nobug_teamproject.models.Book;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class BookServiceTest {
+
+    @Autowired
+    BookService bookService;
 
     @BeforeEach
     void setUp() {
@@ -18,14 +27,23 @@ class BookServiceTest {
 
     @Test
     void searchBook() {
+        List<Book> expectResult = new ArrayList<>();
+        List<Book> searchResult = bookService.searchBook("unit test");
+        assertEquals(expectResult, searchResult);
     }
 
     @Test
     void searchCategory() {
+        List<Book> expectResult = new ArrayList<>();
+        List<Book> searchResult = bookService.searchCategory("unit test");
+        assertEquals(expectResult, searchResult);
     }
 
     @Test
     void getBookId() {
+        List<Book> expectResult = new ArrayList<>();
+        List<Book> searchResult = bookService.getBookId(123);
+        assertEquals(expectResult, searchResult);
     }
 
     @Test
